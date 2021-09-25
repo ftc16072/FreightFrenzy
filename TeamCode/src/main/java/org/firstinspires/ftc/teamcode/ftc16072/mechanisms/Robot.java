201e9e47ad88f5c277af32b5b2cd836dd708e822
Philip <philip@randomsmiths.com>
2021-09-25 13:54:39 -0400

    Added a Robot.java File

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/mechanisms/Robot.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.ftc16072.mechanisms;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Robot {
+
+    public void init(HardwareMap hwmap) {
+
+    }
+
+}
