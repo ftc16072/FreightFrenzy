5eff138e66fe2f84e42f7e5bbfa31498684109df
Philip <philip@randomsmiths.com>
2021-09-25 14:49:10 -0400

    Update robot features to include QQ mechanisms stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/mechanisms/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/mechanisms/Robot.java
@@ -2,10 +2,24 @@ package org.firstinspires.ftc.teamcode.ftc16072.mechanisms;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import java.util.List;
+
 public class Robot {
+    List<QQ_Mechanism> mechanismList;
 
+
+    /**
+     * Automatically initializes all its mechanisms
+     *
+     * @param hwmap the hardware map to init the mechanisms with
+     */
     public void init(HardwareMap hwmap) {
-
+        for (QQ_Mechanism mechanism : mechanismList) {
+            mechanism.init(hwmap);
+        }
     }
 
+    public List<QQ_Mechanism> getMechanismList() {
+        return mechanismList;
+    }
 }
