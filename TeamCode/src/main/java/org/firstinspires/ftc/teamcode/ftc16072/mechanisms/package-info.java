e419e62fb935cc3b5fc9c70dd0277ac8264a2c50
Philip <philip@randomsmiths.com>
2021-09-25 13:46:24 -0400

    Organization and package-info files

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/mechanisms/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package is wehre we store our mechanisms code.
+ * <p>
+ * This is all the code that interacts with the hardware,
+ * and gives us the ability to abstract the math and controls from our opMode
+ */
+
+package org.firstinspires.ftc.teamcode.ftc16072.mechanisms;
