be664d6709938a92eef1992902beed1785600a0b
Philip <philip@randomsmiths.com>
2021-09-25 14:48:13 -0400

    Commenting

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/mechanisms/QQ_Mechanism.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/mechanisms/QQ_Mechanism.java
@@ -8,12 +8,25 @@ import java.util.List;
 
 public abstract class QQ_Mechanism {
 
-
+    /**
+     * forces the mechanism to have an init
+     *
+     * @param hwMap forces the init to take a hardware map
+     */
     public abstract void init(HardwareMap hwMap);
 
+    /**
+     * forces the mechanism to have a getTests();
+     *
+     * @return returns the tests for the mechanism
+     */
     public abstract List<QQ_Test> getTests();
 
-
+    /**
+     * allows us to do things like println(mechanism);
+     *
+     * @return returns the name of the class
+     */
     public String toString() {
         return this.getClass().getName();
     }
