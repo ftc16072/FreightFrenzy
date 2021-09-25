9e7316f1aba178d70a74322e64caef1f688c4476
Philip <philip@randomsmiths.com>
2021-09-25 18:32:20 -0400

    added gamepads to opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/opModes/QQ_Opmode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/opModes/QQ_Opmode.java
@@ -4,10 +4,13 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 import org.firstinspires.ftc.teamcode.ftc16072.actions.QQ_Action;
 import org.firstinspires.ftc.teamcode.ftc16072.mechanisms.Robot;
+import org.firstinspires.ftc.teamcode.ftc16072.utils.QQ_Gamepad;
 
 public abstract class QQ_Opmode extends OpMode {
     public Robot robot = new Robot();
     protected QQ_Action curr;
+    public QQ_Gamepad gp1 = new QQ_Gamepad(gamepad1);
+    public QQ_Gamepad gp2 = new QQ_Gamepad(gamepad2);
 
 
     @Override
@@ -23,5 +26,7 @@ public abstract class QQ_Opmode extends OpMode {
         } else {
             telemetry.addData("State", "Done");
         }
+        gp1.update(gamepad1);
+        gp2.update(gamepad2);
     }
 }
