af172af6dd65b9a556c69a652af9eb18e8fb7967
philip smith <philip@randomsmiths.com>
2022-01-11 17:50:50 -0500

    Added new Intake Code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/mechanisms/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/mechanisms/Intake.java
@@ -16,6 +16,14 @@ public class Intake extends QQ_Mechanism {
 
     DcMotorEx intake2;
 
+    public enum Which{
+        LEFT,
+        RIGHT,
+        BOTH
+    }
+
+
+
     public Intake() {
 
     }
@@ -50,16 +58,47 @@ public class Intake extends QQ_Mechanism {
 
     }
 
-    public void intake() {
-        intake1.setPower(0.8);
-        intake2.setPower(0.8);
+    public void intake(Which which) {
+        switch (which){
+            case BOTH:
+            default:
+                intake1.setPower(0.8);
+                intake2.setPower(0.8);
+                break;
+            case LEFT:
+                intake1.setPower(0.8);
+                break;
+            case RIGHT:
+                intake2.setPower(0.8);
+        }
+
     }
-    public void outtake() {
-        intake1.setPower(-0.4);
-        intake2.setPower(-0.4);
+    public void outtake(Which which) {
+        switch (which){
+            case BOTH:
+            default:
+                intake1.setPower(-0.8);
+                intake2.setPower(-0.8);
+                break;
+            case LEFT:
+                intake1.setPower(-0.8);
+                break;
+            case RIGHT:
+                intake2.setPower(-0.8);
+        }
     }
-    public void off() {
-        intake1.setPower(0);
-        intake2.setPower(0);
+    public void off(Which which) {
+        switch (which){
+            case BOTH:
+            default:
+                intake1.setPower(0);
+                intake2.setPower(0);
+                break;
+            case LEFT:
+                intake1.setPower(0);
+                break;
+            case RIGHT:
+                intake2.setPower(0);
+        }
     }
 }
