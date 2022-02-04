1f26ebbca7663859d2185684e75698d818ca661e
Philip <philip@randomsmiths.com>
2022-02-03 21:44:45 -0500

    Fixed Drive CM bugs and converted Auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/mechanisms/Nav.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/mechanisms/Nav.java
@@ -1,11 +1,15 @@
 package org.firstinspires.ftc.teamcode.ftc16072.mechanisms;
 
+import android.os.Debug;
+import android.util.Log;
+
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Nav {
@@ -23,15 +27,49 @@ public class Nav {
 
     }
 
-    public boolean driveCM(double distance) {
-        double[] currentDistance = robot.driveTrain.getWheelPositions(DistanceUnit.CM);
-        double leftDistance = (currentDistance[0] + currentDistance[1] / 2);
-        double rightDistance = (currentDistance[2] + currentDistance[3] / 2);
-        robot.driveTrain.drive(distance - leftDistance * .2, distance - rightDistance * .2);
-        return currentDistance[4] >= distance - 2;
+    public void resetPosition() {
+        robot.driveTrain.resetWheels();
     }
 
-    public void drivePower(double leftSpeed, double rightSpeed){
+    public boolean driveCM(double distance) {
+        double[] currentDistance = robot.driveTrain.getWheelPositions(DistanceUnit.CM);
+        Log.d("QQ", Arrays.toString(currentDistance));
+        Log.d("QQ", String.valueOf(distance));
+        drivePower(calculateSpeed(distance, currentDistance[4]), calculateSpeed(distance, currentDistance[4]));
+
+        boolean done = isDone(distance, currentDistance[4]);
+
+        if (done) {
+            drivePower(0, 0);
+        }
+
+        return done;
+    }
+
+    private boolean isDone(double goal, double current) {
+        double toleranceLower = Math.signum(goal) * -5;
+        double toleranceUpper = Math.signum(goal) * 5;
+
+        boolean within = (Math.abs(current) >= Math.abs(goal + toleranceLower)) && (Math.abs(current) <= Math.abs(goal + toleranceUpper));
+
+        boolean sameSign = (Math.signum(current) == Math.signum(goal));
+
+        return within && sameSign;
+    }
+
+    private double calculateSpeed(double goal, double current) {
+        double diff = goal - current;
+        if (Math.signum(diff) == 1) {
+            return Math.max(Math.abs(diff * .1), .2);
+        } else {
+            return Math.min(Math.abs(diff * .1), -.2);
+        }
+
+
+    }
+
+    public void drivePower(double leftSpeed, double rightSpeed) {
+        Log.d("QQ", leftSpeed + " : " + rightSpeed);
         robot.driveTrain.drive(leftSpeed, rightSpeed);
     }
 
