f4ce00945a6ef2573565f7759a5fb59520f48481
Philip <philip@randomsmiths.com>
2022-02-12 14:30:11 -0500

    manual rebase onto master

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/opModes/AutoBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/opModes/AutoBlue.java
@@ -11,22 +11,19 @@ import org.firstinspires.ftc.teamcode.ftc16072.actions.DuckSpin;
 import org.firstinspires.ftc.teamcode.ftc16072.actions.GoToSelectedLevel;
 
 @Autonomous
-public class DuckAutoRed extends QQWebcamAuto{
+public class AutoBlue extends QQWebcamAuto{
     @Override
     public void start() {
         super.start();
-        curr = new DriveCM(17, DistanceUnit.INCH)
+        curr = new DriveCM(-27, DistanceUnit.INCH)
                 .setNext(new DuckSpin(true))
                 .setNext(new DelayTill(4))
                 .setNext(new DuckSpin(false))
-                .setNext(new GoToSelectedLevel())
-                .setNext(new DriveCM(-48, DistanceUnit.INCH))
+                .setNext(new DualAction("drive and move lift to the right height", new DriveCM(48, DistanceUnit.INCH), new GoToSelectedLevel()))
                 .setNext(new DropCube(true))
                 .setNext(new DelayTill(.25))
                 .setNext(new DropCube(false))
-                .setNext(new DriveCM(48, DistanceUnit.INCH))
         ;
-
 
     }
 }
