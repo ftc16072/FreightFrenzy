5c6699cb17b0d92480f355113ef5425542c884c8
Joshua-Smith-42 <joshua@randomsmiths.com>
2022-03-06 17:00:14 -0500

    cleaned up with enum

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC16072/mechanisms/Virtual4Bar.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC16072/mechanisms/Virtual4Bar.java
@@ -4,7 +4,6 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.FTC16072.tests.QQTest;
-import org.firstinspires.ftc.teamcode.FTC16072.tests.TestCRServo;
 import org.firstinspires.ftc.teamcode.FTC16072.tests.TestServo;
 
 import java.util.Arrays;
@@ -16,6 +15,12 @@ public class Virtual4Bar extends Mechanism{
     final static double MIDDLE = 0.35;
     final static double BOTTOM = 0.55;
     final static double INTAKE = .95;
+    public enum Position{
+        TOP,
+        MIDDLE,
+        BOTTOM,
+        INTAKE;
+    }
 
 
     @Override
@@ -23,21 +28,23 @@ public class Virtual4Bar extends Mechanism{
         arm = hwMap.get(Servo.class, "v4b");
     }
 
-    public void top(){
-        arm.setPosition(TOP);
+    public void goTo(Position position){
+        switch(position){
+            case TOP:
+                arm.setPosition(TOP);
+                break;
+            case MIDDLE:
+                arm.setPosition(MIDDLE);
+                break;
+            case BOTTOM:
+                arm.setPosition(BOTTOM);
+                break;
+            case INTAKE:
+                arm.setPosition(INTAKE);
+                break;
+        }
     }
 
-    public void middle(){
-        arm.setPosition(MIDDLE);
-    }
-
-    public void bottom(){
-        arm.setPosition(BOTTOM);
-    }
-
-    public void intakePosition(){
-        arm.setPosition(INTAKE);
-    }
 
     @Override
     public List<QQTest> getTests() {
